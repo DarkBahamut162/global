95d8ae5b3465ad097f897f20fb601468bcafd702
DarkBahamut162 <dark_bahamut162@hotmail.com>
2021-09-06 09:23:22 +0200

    Add Center Hold/Roll Head

---
+++ mgd3/Center Hold Head Active.lua
@@ -0,0 +1,7 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('_center','hold active meshes');
+	Materials=NOTESKIN:GetPath('_center','hold active materials');
+	Bones=NOTESKIN:GetPath('_center','tap bones');
+};
+
+return t;
---
+++ mgd3/Center Roll Head Active.lua
@@ -0,0 +1,7 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('_center','roll active meshes');
+	Materials=NOTESKIN:GetPath('_center','roll active materials');
+	Bones=NOTESKIN:GetPath('_center','tap bones');
+};
+
+return t;
