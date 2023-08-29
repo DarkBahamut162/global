aabb28f42852c8bda6214b0415f05000e95123f7
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-08-29 21:51:14 +0200

    Delete Unneeded Stuff

--- smx/Center Pit Head Inactive.lua
+++
@@ -1,8 +0,0 @@
-local t = Def.Model {
-	InitCommand=cmd(ztest,false;basezoom,FIXUP*.75);
-	Meshes=NOTESKIN:GetPath('_pit','model');
-	Materials=NOTESKIN:GetPath('_pit','model');
-	Bones=NOTESKIN:GetPath('_pit','model');
-};
-
-return t;
