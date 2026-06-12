72de72a587c8ddca10a659ac919aa1d03c13f567
DarkBahamut162 <dark_bahamut162@hotmail.com>
2026-06-12 15:37:12 +0200

    Update IIDXAC Stuff

---
+++ IIDX-AC/turntable/_sparkleshower.lua
@@ -0,0 +1,11 @@
+local reverse = ...
+return Def.ActorFrame{
+	Def.Sprite {
+		Texture="texture/sparkleshower.png",
+		Frame0000=0,
+		Delay0000=1,
+		InitCommand=function(self) self:zoom(0.5):addy(reverse and 56 or -56):spin():effectmagnitude(0,0,360) end,
+		PressCommand=function(self) self:finishtweening():effectmagnitude(0,0,-540) end,
+		LiftCommand=function(self) self:finishtweening():effectmagnitude(0,0,360) end
+	}
+}
